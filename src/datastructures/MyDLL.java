1b379cc9bc9696f1fa45ecc0a819fedda517b8aa
Abel Chen <abo-chen@users.noreply.github.com>
2023-11-24 18:39:37 -0700

    java docs

--- src/datastructures/MyDLL.java
+++ src/datastructures/MyDLL.java
@@ -4,11 +4,21 @@ import exceptions.NoSuchElementException;
 import utilities.Iterator;
 import utilities.ListADT;
 
+/**
+ * Custom implementation of a doubly linked list.
+ * This class provides a linked list implementation that conforms to the ListADT interface,
+ * allowing for efficient element insertion and removal at both ends of the list.
+ *
+ * @param <E> the type of elements held in this list
+ */
 public class MyDLL<E> implements ListADT<E> {
     private MyDLLNode<E> head;
     private MyDLLNode<E> tail;
     private int size;
 
+    /**
+     * Constructs an empty doubly linked list.
+     */
     public MyDLL() {
         head = null;
         tail = null;
@@ -17,6 +27,11 @@ public class MyDLL<E> implements ListADT<E> {
 
     // Implement ListADT methods here using MyDLLNode
 
+    /**
+     * Returns an iterator over elements of type {@code E}.
+     *
+     * @return an Iterator.
+     */
     @Override
     public Iterator<E> iterator() {
         return new Iterator<E>() {
@@ -39,11 +54,20 @@ public class MyDLL<E> implements ListADT<E> {
         };
     }
 
+    /**
+     * Returns the number of elements in this list.
+     *
+     * @return the number of elements in this list
+     */
 	@Override
 	public int size() {
 		return size;
 	}
 
+	/**
+     * Removes all the elements from this list.
+     * The list will be empty after this method returns.
+     */
 	@Override
 	public void clear() {
 		head = null;
@@ -51,6 +75,16 @@ public class MyDLL<E> implements ListADT<E> {
 	    size = 0;
 	}
 
+	/**
+     * Adds an element at a specified index in this list.
+     * Shifts the element currently at that position (if any) and any subsequent elements to the right.
+     *
+     * @param index index at which the specified element is to be inserted
+     * @param toAdd element to be inserted
+     * @return {@code true} (as specified by {@link ListADT#add})
+     * @throws NullPointerException if the specified element is null
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
 	@Override
 	public boolean add(int index, E toAdd) throws NullPointerException, IndexOutOfBoundsException {
 	    if (toAdd == null) throw new NullPointerException("Cannot add null element");
@@ -83,11 +117,25 @@ public class MyDLL<E> implements ListADT<E> {
 	}
 
 
+	/**
+     * Adds the specified element to the end of this list.
+     *
+     * @param toAdd element to be appended to this list
+     * @return {@code true} (as specified by {@link ListADT#add})
+     * @throws NullPointerException if the specified element is null
+     */
 	@Override
 	public boolean add(E toAdd) throws NullPointerException {
 		return add(size, toAdd);
 	}
 
+	/**
+     * Adds all of the elements in the specified list to the end of this list.
+     *
+     * @param toAdd list containing elements to be added to this list
+     * @return {@code true} if this list changed as a result of the call
+     * @throws NullPointerException if the specified list is null
+     */
 	@Override
 	public boolean addAll(ListADT<? extends E> toAdd) throws NullPointerException {
 	    if (toAdd == null) throw new NullPointerException("Cannot add null list");
@@ -98,7 +146,13 @@ public class MyDLL<E> implements ListADT<E> {
 	    return true;
 	}
 
-
+	/**
+     * Returns the element at the specified position in this list.
+     *
+     * @param index index of the element to return
+     * @return the element at the specified position in this list
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
 	@Override
 	public E get(int index) throws IndexOutOfBoundsException {
 	    if (index < 0 || index >= size) throw new IndexOutOfBoundsException("Index out of bounds");
@@ -110,6 +164,14 @@ public class MyDLL<E> implements ListADT<E> {
 	}
 
 
+	/**
+     * Removes the element at the specified position in this list.
+     * Shifts any subsequent elements to the left.
+     *
+     * @param index the index of the element to be removed
+     * @return the element that was removed from the list
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
 	@Override
 	public E remove(int index) throws IndexOutOfBoundsException {
 	    if (index < 0 || index >= size) throw new IndexOutOfBoundsException("Index out of bounds");
@@ -137,6 +199,13 @@ public class MyDLL<E> implements ListADT<E> {
 	    return toRemove.getElement();
 	}
 
+	/**
+     * Removes the first occurrence of the specified element from this list, if it is present.
+     *
+     * @param toRemove element to be removed from this list, if present
+     * @return the element that was removed from the list, or null if the list did not contain the element
+     * @throws NullPointerException if the specified element is null
+     */
 	@Override
 	public E remove(E toRemove) throws NullPointerException {
 	    if (toRemove == null) throw new NullPointerException("Cannot remove null element");
@@ -150,6 +219,15 @@ public class MyDLL<E> implements ListADT<E> {
 	    return null;
 	}
 
+	/**
+     * Replaces the element at the specified position in this list with the specified element.
+     *
+     * @param index index of the element to replace
+     * @param toChange element to be stored at the specified position
+     * @return the element previously at the specified position
+     * @throws NullPointerException if the specified element is null
+     * @throws IndexOutOfBoundsException if the index is out of range
+     */
 	@Override
 	public E set(int index, E toChange) throws NullPointerException, IndexOutOfBoundsException {
 	    if (toChange == null) throw new NullPointerException("Cannot set null element");
@@ -164,11 +242,23 @@ public class MyDLL<E> implements ListADT<E> {
 	    return oldElement;
 	}
 
+	/**
+     * Returns {@code true} if this list contains no elements.
+     *
+     * @return {@code true} if this list contains no elements
+     */
 	@Override
 	public boolean isEmpty() {
 	    return size == 0;
 	}
 
+	/**
+     * Returns {@code true} if this list contains the specified element.
+     *
+     * @param toFind element whose presence in this list is to be tested
+     * @return {@code true} if this list contains the specified element
+     * @throws NullPointerException if the specified element is null
+     */
 	@Override
 	public boolean contains(E toFind) throws NullPointerException {
 	    if (toFind == null) throw new NullPointerException("Cannot check null element");
@@ -182,6 +272,13 @@ public class MyDLL<E> implements ListADT<E> {
 	    return false;
 	}
 
+	/**
+     * Returns an array containing all of the elements in this list in proper sequence.
+     *
+     * @param toHold the array into which the elements of this list are to be stored
+     * @return an array containing the elements of this list
+     * @throws NullPointerException if the specified array is null
+     */
 	@Override
 	public E[] toArray(E[] toHold) throws NullPointerException {
 	    if (toHold == null) throw new NullPointerException("Input array cannot be null");
@@ -196,6 +293,11 @@ public class MyDLL<E> implements ListADT<E> {
 	    return toHold;
 	}
 
+	/**
+     * Returns an array containing all of the elements in this list in proper sequence.
+     *
+     * @return an array containing all of the elements in this list
+     */
 	@Override
 	public Object[] toArray() {
 	    Object[] array = new Object[size];
--- src/datastructures/MyQueue.java
+++ src/datastructures/MyQueue.java
@@ -4,13 +4,28 @@ import utilities.QueueADT;
 import exceptions.EmptyQueueException;
 import utilities.Iterator;
 
+/**
+ * A custom implementation of a queue data structure.
+ * This class utilizes a doubly linked list (MyDLL) to implement the QueueADT interface.
+ *
+ * @param <E> the type of elements held in this queue
+ */
 public class MyQueue<E> implements QueueADT<E> {
     private MyDLL<E> list;
 
+    /**
+     * Constructs an empty queue.
+     */
     public MyQueue() {
         list = new MyDLL<>();
     }
 
+    /**
+     * Adds an element to the rear of the queue.
+     *
+     * @param toAdd the element to add
+     * @throws NullPointerException if the specified element is null
+     */
     @Override
     public void enqueue(E toAdd) throws NullPointerException {
         if (toAdd == null) {
@@ -19,6 +34,12 @@ public class MyQueue<E> implements QueueADT<E> {
         list.add(toAdd);
     }
 
+    /**
+     * Removes and returns the element at the front of the queue.
+     *
+     * @return the element at the front of the queue
+     * @throws EmptyQueueException if this queue is empty
+     */
     @Override
     public E dequeue() throws EmptyQueueException {
         if (isEmpty()) {
@@ -27,6 +48,12 @@ public class MyQueue<E> implements QueueADT<E> {
         return list.remove(0);
     }
 
+    /**
+     * Retrieves, but does not remove, the element at the front of the queue.
+     *
+     * @return the element at the front of the queue
+     * @throws EmptyQueueException if this queue is empty
+     */
     @Override
     public E peek() throws EmptyQueueException {
         if (isEmpty()) {
@@ -35,21 +62,40 @@ public class MyQueue<E> implements QueueADT<E> {
         return list.get(0);
     }
 
+    /**
+     * Removes all elements from the queue.
+     */
     @Override
     public void dequeueAll() {
         list.clear();
     }
 
+    /**
+     * Checks if the queue is empty.
+     *
+     * @return true if the queue is empty, false otherwise
+     */
     @Override
     public boolean isEmpty() {
         return list.isEmpty();
     }
 
+    /**
+     * Returns an iterator over the elements in this queue in proper sequence.
+     *
+     * @return an iterator over the elements in this queue
+     */
     @Override
     public Iterator<E> iterator() {
         return list.iterator();
     }
 
+    /**
+     * Compares this queue with another queue for equality.
+     *
+     * @param that the queue to compare with
+     * @return true if the specified queue is equal to this queue
+     */
     @Override
     public boolean equals(QueueADT<E> that) {
         if (this == that) return true;
@@ -58,16 +104,34 @@ public class MyQueue<E> implements QueueADT<E> {
         return list.equals(other.list);
     }
 
+    /**
+     * Returns an array containing all elements in this queue.
+     *
+     * @return an array containing all elements in this queue
+     */
     @Override
     public Object[] toArray() {
         return list.toArray();
     }
 
+    /**
+     * Returns an array containing all elements in this queue; the runtime type of the returned array is that of the specified array.
+     *
+     * @param holder the array into which the elements of the queue are to be stored, if it is big enough; otherwise, a new array of the same runtime type is allocated for this purpose
+     * @return an array containing the elements of the queue
+     * @throws NullPointerException if the specified array is null
+     */
     @Override
     public E[] toArray(E[] holder) throws NullPointerException {
         return list.toArray(holder);
     }
 
+    /**
+     * Checks if the queue is full.
+     * This method is optional and only necessary for fixed length queues. It is not implemented in this class.
+     *
+     * @return always returns false as the queue is not fixed length
+     */
     @Override
     public boolean isFull() {
         // This method is optional and only necessary for fixed length queues.
@@ -76,6 +140,11 @@ public class MyQueue<E> implements QueueADT<E> {
     	return false;
     }
 
+    /**
+     * Returns the number of elements in this queue.
+     *
+     * @return the number of elements in this queue
+     */
     @Override
     public int size() {
         return list.size();
